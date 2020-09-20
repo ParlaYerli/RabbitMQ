a217f102fe28bae330ab51f518d4cbc63cd74384
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-09-20 17:21:37 +0300

    add headers topic

---
+++ RabbitMQ.Consumer4/Program.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Consumer4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+                    channel.QueueDeclare("kuyruk1", false, false, false, null);
+                    Dictionary<string, object> headers = new Dictionary<string, object>();
+                    headers.Add("format", "pdf");
+                    headers.Add("shape", "a4");
+                    headers.Add("x-match", "all");
+                    channel.QueueBind("kuyruk1", "header-exchange", string.Empty, headers);
+                    var consumer = new EventingBasicConsumer(channel);
+                    channel.BasicConsume("kuyruk1", false, consumer);
+                    consumer.Received += (model, e) =>
+                    {
+                        byte[] body = e.Body.ToArray();
+                        var mesaj = Encoding.UTF8.GetString(body);
+                        Console.WriteLine("mesaj alındı" + mesaj);
+                        channel.BasicAck(e.DeliveryTag, multiple: false);// consumer, mesajı işledikten sonra broker queueden silebilir.   
+                    };
+                    Console.WriteLine("Cıkıs yapmak için tıklayın"); 
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ RabbitMQ.Publisher4/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RabbitMQ.Publisher4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory();
+            factory.HostName = "localhost";
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("header-exchange", durable: true, type: ExchangeType.Headers);
+
+                    var properties = channel.CreateBasicProperties();
+                    Dictionary<string, object> headers = new Dictionary<string, object>();
+                    headers.Add("format","pdf");
+                    headers.Add("shape", "a4");
+                    properties.Headers = headers;
+                    Console.WriteLine("mesaj gönderildi");
+                    channel.BasicPublish("header-exchange",string.Empty,properties, Encoding.UTF8.GetBytes("header mesajım"));
+                }
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
